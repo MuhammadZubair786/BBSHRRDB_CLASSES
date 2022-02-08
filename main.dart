795d0b54e8059bbb80abfbab29d1d5917e332e40
MuhammadZubair786 <mzhassan444@gmail.com>
2022-02-08 13:31:54 +0500

    first files

---
+++ main.dart
@@ -0,0 +1,42 @@
+void main(){
+  // print(" Twinkle,  twinkle,  little  star,\n\tHow  I  Wonder  What You  are !\n\t\tUp above the world so high, \n\t\tLike a diamond in the sky.\n\tTwinkle,  twinkle,  little  star,\nHow  I  Wonder  What You  are  ");
+
+  // String FirstName = "Omer";
+  // String LastName = "Khan";
+  // int age = 20;
+  // num salary = 100;
+
+  // print("User Name is ${FirstName}");
+  // print("User Last Name is ${LastName}");
+
+  // print("User Age is is : "+ age.toString());
+  // print("User Salary is  ${salary}");
+
+
+  // print("First Name Type : ${FirstName.runtimeType}");
+  // print("Last Name Type : ${LastName.runtimeType}");
+  // print("Age Type : ${age.runtimeType}");
+  // print("salary Type : ${salary.runtimeType}");
+
+
+  num n1 = 5;
+  num n2 =90;
+
+  num sum = n1+n2;
+  num sub = n1-n2;
+  num multi = n1*n2;
+  num div = n1/n2;
+  num intdiv = n1~/n2;
+
+  print(sum);
+  print(sub);
+  print(multi);
+  print(div);
+  print(intdiv);
+
+
+
+
+
+
+}
---
+++ new.dart
@@ -0,0 +1,148 @@
+void main(){
+  // print()=>predefined  Function =>functionaname()
+  // Comment => (ctrl+/)
+  // Function Type: Predefine Function(build in),UserDefine Function
+  // \n=>new line(escape Character)
+  // \t=>tab(5 spaces)
+//  print("Hello ");
+//  print('World');
+//  print("Hello\nWorld\nclass 2\nfriday ");
+//  print("Hello");
+//  print("World");
+//  print("Class 2");
+//  print("friday");
+// print("Class 2                         Friday");
+// print("class 3 \tClass 4");
+// print("Asad");
+// print("Khan");
+// print("Data");
+// print("Asad");
+// print("Khan");
+// print("Asad");
+// print("Khan");
+// print("Asad");
+// print("Khan");
+
+// varaible=> in memory location store value
+// how to make Variable
+// Rule For MAKE vRAAIBLE
+// NOT START WITH NUMBER
+// NOT START WITH SPECIAL CHARCTER BUT EXCEPT $
+// NOT SPACE BETWEEN THEM VN
+//NUMBER 
+// Datatype=>which type of data store in this varaible
+// var d_ear=123;
+// Declare,inlinlization
+// int a; //Declare Variale
+// int b=12; //Inlinlization
+// Syntax
+// Datatype vraibale;
+// 1)Number =>int,double,num
+// int a1=12; //int not accept point value
+// double a2=14265362535.4554;
+// num a3 = 1253656253652.726472;
+// print(a1);
+// print(a2);
+// print(a3);
+
+// 2)String=>collection of words/letter
+// String b1='Asad sbdhgs shgdhgsfd gsfdgfsgd sgdfgsfdg sgdgs14324132 tert tqrtwr52.2626732';
+// String b3='Omer';
+// print(b1);
+// print(b3);
+
+// 3)boolean =>true,false
+// bool a=false;
+// print(a);
+// var a= true;
+// a=false;
+// a="12";
+// boolean=>true,false
+
+// String a1 = "asad";
+// print(a1);
+// a1='123';
+// print(a1);
+
+
+// bool a2= false;
+// int a3= 12;
+// int a;  //DECLARE
+
+// print(a1.runtimeType);
+// print(a2.runtimeType);
+// print(a3.runtimeType);
+// runtimeType=>property
+
+// String a="asad";
+// print(a);
+
+// a +="omer";
+// print(a);
+            //
+// List  education_list =["HSSC","SSC","BSC","MSC","MPHIL","pHD"];
+// print(education_list[2]);
+// index start 0
+
+// var a ="asad";
+// var age =12;
+
+// print("User Name  : "+ a +"\nAge : "+ a.toString());
+// print("User Name : ${a}\t Age : ${age} ");
+
+// Operator
+// Arithmetic operator(+,-,/,*,%)
+
+// int a =4;
+// int b =2;
+// int sum = a  +  b ;
+
+
+//  // a=>operand
+// // +=>operator
+// // b=>operand
+// print(sum);
+// // Sum of a and b is =>17
+// print("Sum Of " + a.toString() + "and  " + b.toString()  + "is : " + sum.toString());
+// print("Sum Of ${a} and ${b} is : ${sum}");
+// print("Sub Of ${a} and ${b} is : ${a-b}");
+// print("Multi Of ${a} and ${b} is : ${a*b}");
+
+
+// num a1 = 25~/3;  // 2.5
+// print(a1);
+
+  
+// double a2= 63%4 ;   //  1 
+//  print(a2);
+
+// var a = 21%2; //0
+// print(a);
+
+// var a = "12";
+// var b = "3";
+// var sum = a+b;  //36  (121212)
+// print(sum);
+
+// Increment=>pre,post(++)
+// Decrement=>pre ,post(--)
+
+// pre incremnt=>first value add then asign(++varibale name)
+// post  increment =>first assign value then add value 
+// ++=>only add one value
+// var b = 12;
+// print(++b);
+// print(b++);  //12    13
+// print(b); //13
+var c = 10;
+c++; //11
+print(c);
+
+
+
+
+
+
+
+
+}
