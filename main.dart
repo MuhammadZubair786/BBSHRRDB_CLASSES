a226f273d532f21f20a8d60dca447ce4672bdeb1
MuhammadZubair786 <mzhassan444@gmail.com>
2022-02-15 00:17:55 +0500

    Class_5

---
+++ main.dart
@@ -0,0 +1,177 @@
+import 'dart:io';
+
+import 'dart:math';
+
+void main() {
+  // int rnumb  = Random().nextInt(10);
+  // print(rnumb);
+  // stdout.write("Enter Any Number :");
+  // int a = int.parse(stdin.readLineSync()!);
+  // if(rnumb == a){
+  //   print("Equal And Great");
+  // }
+  // else if(rnumb>a){
+  //   print("Less and Incorrect");
+  // }
+  // else if(rnumb<a){
+  //    print("Greater and Incorrect");
+  // }
+//   stdout.write("Enter Your Name Please:");
+//   var name = stdin.readLineSync();
+//   stdout.write("Enter Your Roll Number Please:");
+//   int rl = int.parse(stdin.readLineSync()!);
+//   stdout.write("Enter Your Trade Please:");
+//   var trade = stdin.readLineSync();
+//   stdout.write("Enter Your Subject Marks:");
+//   int s1 = int.parse(stdin.readLineSync()!);
+//   stdout.write("Enter Your Subject Marks:");
+//   int s2 = int.parse(stdin.readLineSync()!);
+//   stdout.write("Enter Your Subject Marks:");
+//   int s3 = int.parse(stdin.readLineSync()!);
+//   int totl = s1 + s2 + s3;
+//   double per = totl / 300 * 100;
+//   if (s1 <= 100 && s2 <= 100 && s3 <= 100) {
+//     print("Your Total Marks: ${totl}\nYour Percentage is${per}");
+//     if (per >= 80) {
+//       print("A Grade");
+//     } else if (per >= 70 && per < 80) {
+//       print("B Grade");
+//     } else if (per >= 60 && per < 70) {
+//       print("C Grade");
+//     } else if (per >= 50 && per < 60) {
+//       print("D Grade:");
+//     } else {
+//       print("Fail");
+//     }
+//   } else {
+//     print("Number should be less than or equal to 100");
+//   }
+// }
+
+  // stdout.write("Enter First Number");
+
+  // int a = int.parse(stdin.readLineSync()!);
+
+  // stdout.write("Enter Second Number");
+
+  // int b = int.parse(stdin.readLineSync()!);
+
+  // stdout.write("Enter Thirds Number");
+
+  // int c = int.parse(stdin.readLineSync()!);
+  // if (a > b && a > c) {
+
+  //   print(" ${a} is Greater");
+
+  //   if (b > c) {
+  //     print("${b} is Medium");
+  //     print("${c} is small");
+    
+  //   }
+
+  //    else if (c > b) {
+  //     print("${c} is Medium");
+  //     print("${b} is small");
+
+  //   }
+  // } 
+  // else if (b > a && b > c) {
+  //   print(" ${b} is Greater");
+
+  //   if (c > a) {
+  //     print("${c} is Medium");
+  //     print("${a}} is small");
+
+  //   }
+
+  //   else if(a>c){
+  //     print("${a} is Medium");
+  //     print("${c} is small");
+
+
+
+  //   }
+  // } else if (c > a && c > b) {
+  //   print(" ${c} is Greater");
+
+  //   if (b > a) {
+  //     print("${b} is Medium");
+  //     print("${a} is small");
+
+  //   }
+
+  //   else if(a>b){
+  //     print("${a} is Medium");
+  //     print("${b} is small");
+
+
+  //   }
+  // }
+
+  // List,String=>index
+
+  // String a = 'Z1abtech HysDGFDGDFG ';
+  // int a1= 123;
+  // print(a[1]);
+  // print(a.toLowerCase());
+
+//   var b ="10";
+// print(b.length);
+
+  // print(a[7]);
+  // properties => .name
+  // method=> .name()
+  // function=>name()
+
+  // Length=>get total  elemnet in string(list)
+  //lower case=>all letter in small case
+  // print(a.length);
+  // print(a.toLowerCase());
+  // print(a.toUpperCase());
+
+ 
+  // stdout.write("Enter Your Name");
+  // var a = stdin.readLineSync()!;
+  // print(a);
+  // print(a.toUpperCase());
+
+
+}
+
+// rnum//2
+// user 6// greater
+// user 2 =>equal
+// user 0 =>less
+
+
+// NAME,ROLL NO,TRADE
+// SUBJECT =>  100
+// 100
+// 39 
+//
+ 
+
+// TOTAL MARK=300
+// OBT = 239
+// PER=>OB/TOTAL*100
+
+// GRADE=>
+// PER>=80 => A+
+// per<80 and per>=70=>A
+// Per<70 AND per>=60 =>B
+// per<60 and PER>=50 =C
+// per<50 and per>40 => D
+// per<40=>Fail
+
+
+// 3 user =>
+
+// 34
+// 12 
+// 54
+
+
+// 54 greater
+// 34 medium
+// 12 small
+
