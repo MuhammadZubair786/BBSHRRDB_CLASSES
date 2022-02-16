a701bbad135f76835a8187b23d6da56eef4cdbf7
MuhammadZubair786 <mzhassan444@gmail.com>
2022-02-16 09:26:23 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,79 @@
+void main() {
+  // 0 1 2 3 4 5 6 7 8
+  // validation(capital,small,number)
+
+  // if(65 to 90 )=>Capital ASadda
+  // (97 to 122)=>small
+  // (48 to 57)=>number
+  // (33 to  47 )=>special
+
+  // print(a.indexOf("Z")); //5
+  // print(a.lastIndexOf("Z")); //15,16
+  // print(a.indexOf("Z",6));
+  // if(a.codeUnitAt(0)>=65 && a.codeUnitAt(0)<=90 &&  a.codeUnitAt(1)>=65 && a.codeUnitAt(1)<=90){
+  //   print("Capital Letter");
+  //  }
+  //  else{
+  //    print("Plz Enter Must Capital index at 0 or 1");
+  //  }
+ 
+
+  // print(a.codeUnits); //get all ascii code
+  // print(a.codeUnitAt(3)); //get spefic index ascii code
+  // Codeunit =>return Ascii value
+
+  // print(a.toLowerCase().contains("m"));  // true
+  // print(b.toUpperCase().contains("M"));  // false
+  // print(c.toUpperCase().contains("M"));  // false
+
+  // print(a.startsWith("A"));
+  // print(b.startsWith("A"));
+  // print(c.startsWith("A"));
+
+  //  String a = "SMIT HYDERBAD";
+  // var c = "Asad";
+
+  // if (c.startsWith("As") == true) {
+  //   print(c);
+  //   print("Get Salary");
+  // } else {
+  //   print("Not Get");
+  // }
+
+  // print(a.endsWith("D"));
+
+// a=a.replaceFirst("D", "E");
+// print(a);
+// print(a.split("")); //split =>list 
+//   String b = "SMIT HYDERBAD";
+
+// var  Name = b.split(" ");      
+// print(Name);
+// var FirstName= Name[0];
+// var LastName = Name[1];
+
+// print(FirstName);
+// print(LastName); 
+
+// zEbteTcH HyDerAbad
+// Zebtech Hyderabad
+            // 0 1 2 3 4 5 6 7 8 
+            // Asad Khan
+            // Ali Minhas
+// String b = "ZebtEch HyDeraBad";
+// // var c = b.substring(1,6);   //D,//ATA  //Ata  ,   ata
+// // print(c);
+// var d = b.substring(0,1).toUpperCase();
+// var e = b.substring(1,8).toLowerCase();
+// var f = b.substring(8,9).toUpperCase();
+// var g = b.substring(9).toLowerCase();
+// print(d+e+f+g);
+
+// print(d);
+
+// Substring=>get String from specific Index(start index,end index -1)
+
+
+
+
+}
