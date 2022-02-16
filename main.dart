0aa3c909dac58d1d77c4834b936cb516fb09587a
MuhammadZubair786 <mzhassan444@gmail.com>
2022-02-16 23:12:13 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+void main(){
+//  int num1 = int.parse(stdin.readLineSync()!);
+//  int num2 = int.parse(stdin.readLineSync()!);
+//  var op = stdin.readLineSync();
+// //  + => Concatenation , $=>interploation
+//  if(op=="+"){
+//    print("Value of A :   ${num1} \n Value of B :${num2} \n Sum Of A+B : ${num1+num2} " );
+
+//  } 
+//  else if(op=="-"){
+//    print("Value of A :   ${num1} \n Value of B :${num2} \n Sub Of A-B : ${num1-num2} " );
+
+//  } 
+//  else if (op=="*"){
+
+//    print("Value of A :   ${num1} \n Value of B :${num2} \n Product Of A*B : ${num1*num2} " );
+
+//  } 
+//  else if (op=="/"){
+
+//    print("Value of A :   ${num1} \n Value of B :${num2} \n Divide Of A+B : ${num1/num2} " );
+
+//  } 
+//  else{
+//    print("Enter Correct Op");
+//  }
+// stdout.write("Enter Any Character To check Ascii code ");
+// String str_Data =  stdin.readLineSync()!;
+
+// if(str_Data.length==1){
+//   print(str_Data.codeUnitAt(0));
+//   if(str_Data.codeUnitAt(0)>=65 && str_Data.codeUnitAt(0)<=90){
+//     print("Capital Letter");
+//   }
+//   else if(str_Data.codeUnitAt(0)>=97 && str_Data.codeUnitAt(0)<=122){
+//     print("Small  Letter");
+//   }
+//   else if (str_Data.codeUnitAt(0)>=48 && str_Data.codeUnitAt(0)<=57){
+//     print("Number");
+//   }
+
+// }
+// else{
+//   print("Enter Only One Character");
+//   print(str_Data.codeUnits);
+// }
+
+// 90
+// String str1= "ZEBTA";
+// // 72
+// String str2="ZEBTC";
+// print(str1.compareTo(str2));
+
+
+}
