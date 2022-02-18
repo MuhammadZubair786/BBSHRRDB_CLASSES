fa01c5ed9c9304d6d2b1a3140d4d281776903bb0
MuhammadZubair786 <mzhassan444@gmail.com>
2022-02-18 23:55:26 +0500

    add new message

---
+++ main.dart
@@ -0,0 +1,29 @@
+void main(){
+  // String a="" ;
+ 
+  // if(a.isEmpty){
+  //   print("Enter Enter Your Email");
+  // }
+  // else{
+  //   print("get email");
+
+  // }
+// OWAIS KHAN
+
+  // String a = "omer khan";
+  // // (start,end-1)
+  // a=a.replaceRange(1, 4, "wais");
+  // print(a);
+  // String a = "Zebtech Hyderabad ";
+  // print(a.allMatches("Zebtech Hyderabad "));
+//  a = a.padLeft(a);
+// print(a.padRight(4));
+  // print(a.trim());
+  // print(a.trimRight());
+  // trim=>Remove From Left or right
+
+
+
+
+  
+}
