4b1e03360ae112a654bea617924a10bbfec65b31
MuhammadZubair786 <mzhassan444@gmail.com>
2022-02-21 16:27:20 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+
+void main() {
+  List a = [142442442 , true, 3,"asad", 45, 8];
+  // a.replaceRange(2, 4,["gdgdgd","djjdjd","djdjj"]);
+  // print(a[4]);
+  // print(a.elementAt(4));
+  // print(a);
+  // a.fillRange(2,3,"asad");
+  // print(a);
+  // print(a.indexOf(89)); /
+  // 45
+  // bool a1 = a.contains(3);
+  // print(a1); //
+  // print(a.indexWhere((e) => e==5));
+  // a.clear();
+  // print(a);
+  // 1=>
+  // 2=>
+  // a.replaceRange(2,3,["asad","omer","ali","smit"]);
+  // a.setRange(2,4,["asad","omer","ali","smit"]);
+  // print(a);
+  // List data = ["omer", "ali", "Asad", "Zohaib"];
+
+  // print(" 1) Replace Range :");
+  // print("2) Set Range : ");
+
+  // stdout.write("Enter Any Number :");
+  // int User_inp = int.parse(stdin.readLineSync()!);
+
+  // if (User_inp == 1) {
+  //   //1
+  //   print("Replace range Method");
+
+  //   stdout.write("Starting Index");
+  //   int inp1 = int.parse(stdin.readLineSync()!);
+
+  //   stdout.write("Ending Index");
+  //   int inp2 = int.parse(stdin.readLineSync()!);
+
+  //   if (inp1 > data.length || inp2 > data.length) {
+  //     print("enter Correct Index Number");
+  //   } else if (inp2 > inp1) {
+  //     stdout.write("Enter Value For add");
+  //     String output = stdin.readLineSync()!;
+  //     data.replaceRange(inp1, inp2, [output]);
+  //     print(data);
+  //   } else if (inp1 > inp2) {
+  //     stdout.write("Enter Value For add");
+  //     String output = stdin.readLineSync()!;
+  //     data.replaceRange(inp2, inp1, [output]);
+  //     print(data);
+  //   } else {
+  //     print("Enter Correct data ");
+  //   }
+  // } else if (User_inp == 2) {
+  //   //2
+  //   print("set Range");
+
+  //   print("Set range Method");
+
+  //   stdout.write("Starting Index");
+  //   int inp1 = int.parse(stdin.readLineSync()!);
+
+  //   if (inp1 > data.length) {
+  //     print("enter Correct Index Number");
+  //   } else if (inp1 < data.length) {
+  //     stdout.write("Enter Value For add");
+  //     String output = stdin.readLineSync()!;
+  //     data.replaceRange(inp1, inp1 + 1, [output]);
+  //     print(data);
+  //   } else {
+  //     print("Enter Correct data ");
+  //   }
+  // } else {
+  //   print("enter Correct Data ");
+  // }
+
+  // print(a.join(" "));
+  // var a1 = "zebtech";
+  // print(a1.split("e"));
+  
+
+}
