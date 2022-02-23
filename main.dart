cce3d30e2d0007d6d4492ef4d3de0530a11d9961
MuhammadZubair786 <mzhassan444@gmail.com>
2022-02-23 09:25:26 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,48 @@
+void  main(){
+  List A = [1,2,3];
+  List B =["smit","Faith","Hist"];
+  // print(A.remove(3));
+  // print(A);
+  // print(A.removeAt(2));
+  // print(A);
+  // A.sort(); //return nothing
+  // print(A);
+  // print(A.sort());
+
+// A.removeRange(2, 20);
+// print(A);
+// print(A.getRange(2, 4));
+// print(A.removeLast()); 
+// print(A);
+// var b ;
+// A.removeWhere((m) => m==3 || m==4 ||  m==56);
+// print(A);
+
+
+// print(A);
+// A.retainWhere((element) => element==3 || element==4 || element==56);
+// print(A);
+// var a =A.skip(4).toList();
+// print(a);
+// print(A);
+// var a = A.take(20);
+// print(A.take(20));
+// print(B.every((element) =>  element.toString().startsWith("F")));
+// A.shuffle();
+// print(A);
+// print(A.sublist(2,4));
+
+// print(A.reduce((value, element) => value-element));
+// print(A);
+
+// List 
+// user Input 
+// => 1 => add
+//2 =>remove
+// 3=>replace
+// 1=>value
+// 2=>remove first,remove last,specfic range
+// 3=>replace range
+
+
+}
